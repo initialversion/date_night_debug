177d857be2f26aa5655180edc1f27b50a4699bd7
Murugan <murugu2012@gmail.com>
2017-05-17 11:49:15 +0530

    Whitelist columns in Activity for ActiveAdmin

--- app/admin/activity.rb
+++ app/admin/activity.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Activity do
 
+ permit_params :location, :name, :description, :website, :yelp_link, :contributor_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
